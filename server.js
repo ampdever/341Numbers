22c5fd8fc019a7478b4694bf77fac989dfc67b09
ampdever <Mag23019@byui.edu>
2024-07-17 17:28:16 -0600

    changed the headers for the server.js

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested_With, Content-Tpe, Accept, Z-Key'
-    );
-    res.setHeader('Access-Control_Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    // res.setHeader(
+    //     'Access-Control-Allow-Headers',
+    //     'Origin, X-Requested_With, Content-Tpe, Accept, Z-Key'
+    // );
+    // res.setHeader('Access-Control_Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
 });
 app.use('/', require('./routes'));
